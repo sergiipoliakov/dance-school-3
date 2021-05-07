d0d722c6f10e17b3362bbc55062597fcc4cfec0d
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2021-05-07 15:34:46 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -407,7 +407,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60086" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65000" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
