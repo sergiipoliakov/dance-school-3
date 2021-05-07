518c7a0459f9cca7f302191dc8be5badd5974839
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2021-05-07 15:09:14 +0300

    Updates

--- menu.234110a1.js
+++ menu.234110a1.js
@@ -160,7 +160,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53930" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58413" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
