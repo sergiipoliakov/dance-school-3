20d53ff3c193654054147f4bbdcfac9f93127894
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2021-07-03 00:13:17 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -197,43 +197,43 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = void 0;
 var _default = [{
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/36/11-36-11-901_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/36/11-36-11-901_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/20/35/20-35-32-857_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/20/35/20-35-32-857_1280.jpg',
   name: 'Елена Головань',
   description: 'Hokkaido Flower'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-33-539_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-33-539_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/03/21-03-41-635_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/03/21-03-41-635_1280.jpg',
   name: 'Елена Головань',
   description: 'Container Haulage Freight'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-33-733_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-33-733_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/03/21-03-26-348_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/03/21-03-26-348_1280.jpg',
   name: 'Елена Головань',
   description: 'Aerial Beach View'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-43-518_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-43-518_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/04/21-04-06-33_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/04/21-04-06-33_1280.jpg',
   name: 'Елена Головань',
   description: 'Flower Blooms'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-43-431_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-43-431_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/11/21-11-33-619_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/11/21-11-33-619_1280.jpg',
   name: 'Елена Головань',
   description: 'Alpine Mountains'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-52-862_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-52-862_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/04/21-04-15-976_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/04/21-04-15-976_1280.jpg',
   name: 'Елена Головань',
   description: 'Mountain Lake Sailing'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-52-795_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/43/11-43-52-795_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/11/21-11-06-816_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/11/21-11-06-816_1280.jpg',
   name: 'Елена Головань',
   description: 'Alpine Spring Meadows'
 }, {
-  preview: 'https://cdn.pixabay.com/photo/2021/05/07/11/47/11-47-46-32_640.jpg',
-  original: 'https://cdn.pixabay.com/photo/2021/05/07/11/47/11-47-46-32_640.jpg',
+  preview: 'https://cdn.pixabay.com/photo/2021/07/02/21/10/21-10-48-249_640.jpg',
+  original: 'https://cdn.pixabay.com/photo/2021/07/02/21/10/21-10-48-249_1280.jpg',
   name: 'Елена Головань',
   description: 'Lighthouse Coast Sea'
 }];
@@ -442,7 +442,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59984" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51327" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
