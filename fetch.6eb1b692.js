939739abc8ffd67778194ce42a24d71d58b2bbdc
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2021-09-29 15:56:36 +0200

    Updates

--- fetch.6eb1b692.js
+++ fetch.6eb1b692.js
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62775" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58130" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
